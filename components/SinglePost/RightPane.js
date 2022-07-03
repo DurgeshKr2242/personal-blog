293312d5bca4ad1c94bdedba9c210a88caf7500c
Durgesh Kumar <code.durgesh@gmail.com>
2022-07-04 03:55:03 +0530

    updating env variables

--- components/SinglePost/RightPane.js
+++ components/SinglePost/RightPane.js
@@ -3,10 +3,10 @@ import SingleRelatedPost from "./SingleRelatedPost";
 
 const RightPane = () => {
   return (
-    <div className="flex tablet:flex-col flex-row gap-8 max-w-[200px] ">
+    <div className="flex max-w-[200px] flex-row gap-8 tablet:flex-col ">
       {/* Realted post section */}
 
-      <div className="flex flex-col gap-0 shadow-md bg-gray-200/30 dark:bg-gray-900/30 rounded-2xl">
+      <div className="flex flex-col gap-0 shadow-md rounded-2xl bg-gray-200/30 dark:bg-gray-900/30">
         <div className="flex flex-col  gap-3 border-l-8 border-black pt-[1px] pb-3 pl-3 dark:border-white">
           <p className="font-bold">Related Posts</p>
         </div>
@@ -15,7 +15,7 @@ const RightPane = () => {
 
       {/* Tags */}
 
-      <div className="flex flex-col gap-0 shadow-md bg-gray-200/30 dark:bg-gray-900/30 rounded-2xl">
+      <div className="flex flex-col gap-0 shadow-md rounded-2xl bg-gray-200/30 dark:bg-gray-900/30">
         <div className="flex flex-col  gap-3 border-l-8 border-black pt-[1px] pb-3 pl-3 dark:border-white">
           <p className="font-bold">Tags</p>
         </div>
